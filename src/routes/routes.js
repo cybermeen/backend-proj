46e94d964705d150728b8a19bf340d108cf7f552
cybermeen <zarmeenrahman.9@gmail.com>
2026-08-06 12:56:52 +0500

    new controllers registered

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,8 +2,15 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
+const purchaseController = require('../controllers/purchaseController');
+const productController = require('../controllers/productController');
+const orderController = require('../controllers/orderController');
 
 router.use('/auth', authController);
 router.use('/users', userController);
+
+router.use('/purchases', purchaseController);
+router.use('/products', productController);
+router.use('/orders', orderController);
 
 module.exports = router;
